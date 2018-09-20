a673f4d469ba2d08f8c587333f51b5f0116b749d
Felix Knorr <felix.knorr@tu-dresden.de>
2018-09-20 15:12:57 +0200

    Release preparations

--- pyparadigm/__init__.py
+++ pyparadigm/__init__.py
@@ -1,8 +1,1 @@
-"""Contains tools that come in handy when building paradigms with PyGame
-
-Modules:
-    frame_builder: can be used to create complex frames in a declarative way
-
-"""
-
-__version__ = '0.1'
+__version__ = '0.9'
---
+++ readme.rst
@@ -0,0 +1,7 @@
+PyParadigm
+==========
+
+Welcome to PyParadigm. You can find the documentation including an introduction
+and a tutorial over at: 
+
+We have a git-repository too: https://github.com/KnorrFG/pyparadigm
