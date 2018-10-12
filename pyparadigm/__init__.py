d90c86ebe51aeee82868831f84c07a5307676ebc
Felix Knorr <felix.knorr@tu-dresden.de>
2018-10-12 09:15:34 +0200

    Added .ropeproject to gitignore and made everything importable from pyparadigm

--- pyparadigm/__init__.py
+++ pyparadigm/__init__.py
@@ -1,2 +1,10 @@
 """A package to build psychological Paradigms. Based on PyGame"""
 __version__ = '1.0.1'
+
+
+from .eventlistener import EventConsumerInfo, EventListener
+from .misc import init, display, empty_surface, slide_show
+from .surface_composition import Border, Circle, Fill, LLItem, LinLayout,\
+                                 Line, Margin, Overlay, Padding,\
+                                 RectangleShaper, Surface, Cross, Font,\
+                                 GridLayout, Text, compose
